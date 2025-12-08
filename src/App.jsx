1168e374aec4d61bce0b0a62fe6e1f6ec78dc09d
Jaydin McMullan <jaydin.m@hotmail.com>
2025-12-08 21:56:28 +1300

    Updated file paths, and added to webapp config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Seeds from "./pages/Seeds";
-import Plots from "./pages/Plots";
-import About from "./pages/About";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./pages/home";
+import Seeds from "./pages/seeds";
+import Plots from "./pages/plots";
+import About from "./pages/about";
 
 function App() {
   return (
