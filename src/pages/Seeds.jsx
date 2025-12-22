d211a5df7374e64951dfbfffab760c72f3fc881d
Jaydin McMullan <jaydin.m@hotmail.com>
2025-12-23 11:28:26 +1300

    ui: close seed modal with escape key

--- src/pages/Seeds.jsx
+++ src/pages/Seeds.jsx
@@ -86,6 +86,18 @@ function Seeds() {
     };
   }, [isFormOpen]);
 
+  useEffect(() => {
+    if (isFormOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isFormOpen]);
+
   return (
     <>
       <button
