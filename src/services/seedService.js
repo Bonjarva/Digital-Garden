baed8de09be976dffa0e64f64f37fdb3428563cb
Jaydin McMullan <jaydin.m@hotmail.com>
2025-12-24 19:02:53 +1300

    chore: add seed service skeleton

---
+++ src/services/seedService.js
@@ -0,0 +1,11 @@
+export function createSeed({ title, description, plotId }) {
+  // logic will go here
+}
+
+export function updateSeed(seed) {
+  // logic will go here
+}
+
+export function deleteSeed(seeds, seedId) {
+  // logic will go here
+}
