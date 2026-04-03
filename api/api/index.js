066e08c17947f4ea4df9b18fd9d154a30f0faa0d
Jaydin McMullan <jaydin.m@hotmail.com>
2026-04-04 00:47:16 +1300

    404 error on `/api/listSeeds` by ensuring all Azure Functions are explicitly registered using a standard Node.js v4 entry point.

---
+++ api/api/index.js
@@ -0,0 +1,5 @@
+﻿const { app } = require("@azure/functions");
+require("./src/functions/listSeeds");
+require("./src/functions/createSeed");
+require("./src/functions/updateSeed");
+require("./src/functions/deleteSeed");
