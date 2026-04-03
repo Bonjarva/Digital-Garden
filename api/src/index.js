2bcc5c67f296f077f57e086878ac24c3b154186c
Jaydin McMullan <jaydin.m@hotmail.com>
2026-04-03 22:40:49 +1300

    - Fixed a 404 error on the live Azure environment by ensuring proper discovery and registration of backend functions.

---
+++ api/src/index.js
@@ -0,0 +1,5 @@
+﻿const { app } = require("@azure/functions");
+require("./functions/listSeeds");
+require("./functions/createSeed");
+require("./functions/updateSeed");
+require("./functions/deleteSeed");
